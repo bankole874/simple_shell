bbbaf54d7d782a8e047be3a7167c14a2cc3ae733
bankole874 <damilarehammed.hd@gmail.com>
2023-11-10 11:11:30 -0800

    added hm_exec_builtin.c

---
+++ hm_exec_builtin.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+
+/**
+* execBuiltin - runs built-in command.
+* @args: first argument.
+* @global: second argument.
+* Return: 1
+*/
+
+int execBuiltin(char **args, global_t *global)
+{
+	char	*cmd = args[0];
+
+	if (!cmd)
+		return (0);
+	else if (!_strncmp("env", cmd, _strlen("env") + 1))
+	{
+		env(args, global);
+		return (1);
+	}
+	else if (!_strncmp("cd", cmd, _strlen("cd") + 1))
+	{
+		cd(args, global);
+		return (1);
+	}
+	else if (!_strncmp("exit", cmd, _strlen("exit") + 1))
+	{
+		my_exit(args, global);
+		return (1);
+	}
+	else if (!_strncmp("setenv", cmd, _strlen("setenv") + 1))
+	{
+		_setenv(args, global);
+		return (1);
+	}
+	else if (!_strncmp("unsetenv", cmd, _strlen("unsetenv") + 1))
+	{
+		_unsetenv(args, global);
+		return (1);
+	}
+	else if (!_strncmp("alias", cmd, _strlen("alias") + 1))
+	{
+		_alias(args, global);
+		return (1);
+	}
+	return (0);
