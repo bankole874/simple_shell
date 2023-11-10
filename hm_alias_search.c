2b6d7a4612882ca1f4b50ec755b1245d5fb8dd78
bankole874 <damilarehammed.hd@gmail.com>
2023-11-10 09:25:42 -0800

    added hm_alias_search.c

---
+++ hm_alias_search.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+
+/**
+ * the_alias_search - Finds an alias in the global alias list
+ * @str: The name of the alias to find
+ * @global: Pointer to the global env
+ * Return: int, NULL otherwise
+ */
+
+char *the_alias_search(char *str, global_t *global)
+{
+	alias_t	*ptr;
+
+	ptr = global->alias;
+	while (ptr)
+	{
+		if (ptr->name && !_strncmp(str, ptr->name, _strlen(str) + 1))
+			return (ptr->value);
+		ptr = ptr->next;
+	}
+	return (0);
+}
+/**
+ * thealiassearch - Finds an alias in the global alias list
+ * @str: The name of the alias to find.
+ * @global: Pointer to the global env
+ * Return: Pointer, NULL
+ */
+
+alias_t	*thealiassearch(char *str, global_t *global)
+{
+	alias_t	*ptr;
+
+	ptr = global->alias;
+	while (ptr)
+	{
+		if (ptr->name && !_strncmp(str, ptr->name, _strlen(str) + 1))
+			return (ptr);
+		ptr = ptr->next;
+	}
+	return (0);
+}
