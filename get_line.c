a763648c79b89c1eff16dc48a7e23c0014fe986d
mayrhiarm <mariamsanni93@gmail.com>
2023-11-10 09:49:04 -0800

    task

---
+++ get_line.c
@@ -0,0 +1,109 @@
+#include "shell.h"
+/**
+ * _All - It Read from fd.
+ * @fd: arg 1.
+ * @left: arg 2.
+ * Return: The concatenated string.
+ */
+static char	*_All(int fd, char *left)
+{
+	char	*buffer;
+	int		readed;
+
+	readed = 1;
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	while (readed && !_my_str_chrc(left, '\n'))
+	{
+		readed = read(fd, buffer, BUFFER_SIZE);
+		if (readed == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[readed] = '\0';
+		left = _my_str_conc(left, buffer);
+	}
+	free(buffer);
+	return (left);
+}
+
+/**
+ * the_get_line - It extract one line.
+ * @line: arg 1.
+ * Return: The extracted line
+ */
+static char	*the_get_line(char *line)
+{
+	int		i;
+	char	*ptr;
+
+	i = 0;
+	if (!line)
+		return (NULL);
+	if (!line[i])
+		return (NULL);
+	while (line[i] && line[i] != '\n')
+		i++;
+	ptr = malloc(i + 2);
+	if (!ptr)
+		return (NULL);
+	_my_memcpy(ptr, line, i);
+	if (line[i] == '\n')
+	{
+		ptr[i] = '\n';
+		ptr[i + 1] = '\0';
+	}
+	else
+		ptr[i] = '\0';
+	return (ptr);
+}
+
+/**
+ * _ft_left - A function that extract the remaining part of the str.
+ * @line: arg 1..
+ * Return: The remaining part of the str.
+ */
+static char	*_ft_left(char *line)
+{
+	int		i;
+	char	*ptr;
+
+	i = 0;
+	if (!line)
+		return (NULL);
+	if (!line[i])
+		return (NULL);
+	while (line[i] && line[i] != '\n')
+		i++;
+	ptr = malloc(_my_str_len(line) - i + 1);
+	if (!ptr)
+		return (NULL);
+	_my_memcpy(ptr, line + i + 1, _my_str_len(line) - i);
+	ptr[_my_str_len(line) - i] = '\0';
+	return (ptr);
+}
+
+/**
+ * _get_line - A func that read a line from fd.
+ * @fd: arg 1.
+ * @global: arg 2.
+ * Return: The next line from the file descriptor,
+ * or NULL on failure or end of file.
+ */
+char	*_get_line(int fd, global_t *global)
+{
+	static char	*left;
+	char		*line;
+	char		*next_line;
+
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	line = all(fd, left);
+	next_line = the_get_line(line);
+	left = _ft_left(line);
+	global->left = left;
+	free(line);
+	return (next_line);
+}
