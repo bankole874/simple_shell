99b96de9e206fe4ccf7589ab1044850c1860a5ad
bankole874 <damilarehammed.hd@gmail.com>
2023-11-10 22:22:35 -0800

    updated hm_errors.c

--- hm_errors.c
+++ hm_errors.c
@@ -1,36 +1,37 @@
 #include "shell.h"
 /**
- * printError - Prints error message
- * @cmd: command where the error occurred
- * @msg: error message to be printed
- * @global: A pointer to the global struct
- * Return: 1
+ * printError - Prints an error message to the standard error output
+ * @cmd: The name of the command where the error occurred
+ * @msg: The error message to be displayed
+ * @global: A pointer to the global state struct
+ * Return: Always return 1
  */
 int	printError(char *cmd, char *msg, global_t *global)
 {
-	write(2, global->name, _strlen(global->name));
+	write(2, global->name, _Str_len(global->name));
 	write(2, ": ", 2);
-	putnbr_fd(global->n, 2);
+	Print_num_fd(global->n, 2);
 	write(2, ": ", 2);
-	write(2, cmd, _strlen(cmd));
+	write(2, cmd, _Str_len(cmd));
 	write(2, ": ", 2);
-	write(2, msg, _strlen(msg));
+	write(2, msg, _Str_len(msg));
 	write(2, "\n", 1);
 	return (1);
 }
 
 /**
- * _print - prints a string to a given file descriptor
- * @str: string
- * @fd: file descriptor
- * @new: flag indicating whether to append a newline  or not
- * Return: int
+ * _print - Writes a string to a specified file descriptor
+ * @str: The string to be written
+ * @fd: The specified file descriptor
+ * @new: A flag indicating whether to append a newline  or not
+ * Return: The numberof char written (excluding the '\n' char if written)
  */
 
 int _print(char *str, int fd, int new)
 {
-	write(fd, str, _strlen(str));
+	write(fd, str, _Str_len(str));
 	if (new)
 		return (write(fd, "\n", 1));
 	return (1);
 }
+
